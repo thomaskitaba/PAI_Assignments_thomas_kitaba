074485e648eaa0eeebf18c21d70072f4299e245e
thomaskitaba <thomas.kitaba@gmail.com>
2024-12-19 22:40:44 +0300

    dynamic_road_conditions.py

--- 2-PAI_Assignment_2/dynamic_road_conditions.py
+++ 2-PAI_Assignment_2/dynamic_road_conditions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random 
 from collections import deque
-
+import sys
 def uninformed_path_finder(cities, roads, start_city, goal_city, strategy):
     """
     Parameters:
@@ -30,16 +30,15 @@ def uninformed_path_finder(cities, roads, start_city, goal_city, strategy):
             info[f"{strategy} Path"] = list(rec_path)
             info["with cost"] = distance
             all_paths.append(info)
-            visited.remove(start)
             rec_path.pop()  # go one step back in your path
-            # visited.add(start)
+            visited.remove(start)
             return # Backtrack
         
         for neigbors in roads.get(start, []):
             
             value = neigbors[1] if len(neigbors) == 2 else 1 # if we have no weight we use 1 as number of road
             dfs(neigbors[0], goal, rec_path, value + distance)
-        visited.remove(start)
+        visited.remove(start) 
         rec_path.pop()
     
     def bfs(stat, end, path, distance):
@@ -57,30 +56,35 @@ roads = {
     'Bahir Dar': [('Addis Ababa', 510), ('Gondar', 180)],
     'Gondar': [('Bahir Dar', 180), ('Mekelle', 300)],
     'Hawassa': [('Addis Ababa', 275)],
-    'Mekelle': [('Gondar', 300), ('Hawassa', 275)]
+    'Mekelle': [('Gondar', 300), ("Hawassa", 100)]
 }
 
 # select random city
-start_city = cities[random.randint(0, len(cities) - 1)]
-goal_city = cities[random.randint(0, len(cities) - 1)]
 
-start_city = "Addis Ababa"
-goal_city = "Hawassa"
+if __name__ == "__main__":
+        
+    start_city = "Mekelle"
+    goal_city = "Hawassa"
 
-print(f'{start_city} to {goal_city}')
-strategy = "DFS"
-# initialize all_paths list and visited set
-all_paths = []
-visited = set()
+    print(f'{start_city} to {goal_city}')
+    strategy = "DFS"
+    # initialize all_paths list and visited set
+    all_paths = []
+    visited = set()
 
-# find shortest path from start to goal city using uninformed search
-uninformed_path_finder(cities, roads, start_city, goal_city, strategy)
-print(all_paths)
-# get the shortest path among all the other paths
-if all_paths:
-    shortest_path = [path for path in all_paths if path["with cost"] == min(list(d["with cost"] for d in all_paths)) ]
-    print(shortest_path)
-else:
-    print("No Path found")
+    # find shortest path from start to goal city using uninformed search
+    uninformed_path_finder(cities, roads, start_city, goal_city, strategy)
+    # get the shortest path among all the other paths
+    sorted_all_paths = sorted(all_paths, key=lambda x: x["with cost"])
 
-# print(f'visited: {visited}')
+    i = eval(sys.argv[1])
+    k = len(all_paths) if i > len(all_paths) else i
+    
+    if i > len(all_paths):
+        print("Usage: provide interger for number of shortest paths")
+        print("all shortes pathers are")
+    print(f"k = {k} shortest paths")
+    for i in range(k):
+        print(sorted_all_paths[i])
+
+
